c0a63790b644c9344636d948a10d2840e82b351e
joneirikboska <joneirikboska@gmail.com>
2020-12-13 16:39:03 +0100

    set noUnusedLocals and noImplicitReturns to true.

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,15 @@
 "use strict";
 let age;
+let appId = "abc";
 age = 31;
 const userName = "test-user";
 console.log(userName);
+function add(n1, n2) {
+    if (n1 + n2 > 0) {
+        return n1 + n2;
+    }
+    return;
+}
 function clickHandler(message) {
     console.log("click! " + message);
 }
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,22 @@
 let age: number
+let appId = "abc"
 age = 31
 
 const userName = "test-user"
 
 console.log(userName)
 
+function add (n1: number, n2: number) {
+    if (n1 + n2 > 0) {
+        return n1 + n2
+    } 
+    // noImplicitReturns option enabled; all paths must lead to a return statement 
+    return
+}
+
 function clickHandler(message: string) {
+    // UNUSED LOCAL VARIABLE
+    // let userName = "JE"
     console.log("click! " + message)
 }
 
