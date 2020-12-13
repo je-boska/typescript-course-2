dc6b3eccfd3a53379c4690db91211e86e28256a0
joneirikboska <joneirikboska@gmail.com>
2020-12-13 16:01:45 +0100

    Specify lib defaults in tsconfig.

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ let age;
 age = 31;
 const userName = "test-user";
 console.log(userName);
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    console.log("Clicked!");
+});
--- app.ts
+++ app.ts
@@ -4,3 +4,9 @@ age = 31
 const userName = "test-user"
 
 console.log(userName)
+
+const button = document.querySelector("button")!
+
+button.addEventListener("click", () => {
+    console.log("Clicked!")
+})
